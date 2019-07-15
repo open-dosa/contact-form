ade02113c03f690d853426b5645a99190cc14d22
thomaspaulson <thomas.paulson@hotmail.com>
2019-07-15 18:21:12 +0530

    error fix - after moving files

--- src/ContactFormServiceProvider.php
+++ src/ContactFormServiceProvider.php
@@ -6,11 +6,11 @@ use Illuminate\Support\ServiceProvider;
 class ContactFormServiceProvider extends ServiceProvider {
 	public function boot()
 	{
-	    $this->loadRoutesFrom(__DIR__.'../routes/web.php');
+	    $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
 		
-	    $this->loadViewsFrom(__DIR__.'../resources/views', 'contactform');
+	    $this->loadViewsFrom(__DIR__.'/../resources/views', 'contactform');
 		
-	    $this->loadMigrationsFrom(__DIR__.'../database/migrations');
+	    $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
 
 	}
